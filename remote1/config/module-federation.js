65901c154fd2a1aa5375f3e7b847b7e025d90c74
Tuncay Kaplan <trkaplan@gmail.com>
2025-05-26 10:13:48 +0300

    initial commit

---
+++ remote1/config/module-federation.js
@@ -0,0 +1,52 @@
+const deps = require('../package.json').dependencies;
+const { ModuleFederationPlugin } = require('@module-federation/enhanced/rspack');
+
+module.exports = {
+  client: new ModuleFederationPlugin({
+    name: 'remote1',
+    filename: 'remoteEntry.js',
+    remotes: {
+      remote2: 'remote2@http://localhost:3002/client/remoteEntry.js',
+    },
+    exposes: {
+      './Content': './src/Content',
+    },
+    shared: {
+      ...deps,
+      react: {
+        singleton: true,
+        requiredVersion: deps.react,
+      },
+      'react-dom': {
+        singleton: true,
+        requiredVersion: deps['react-dom'],
+      },
+    },
+  }),
+  server: [
+    new ModuleFederationPlugin({
+      runtimePlugins: [require.resolve('@module-federation/node/runtimePlugin')],
+      remoteType: 'script',
+      name: 'remote1',
+      filename: 'remoteEntry.js',
+      library: { type: 'commonjs-module' },
+      remotes: {
+        remote2: 'remote2@http://localhost:3002/server/remoteEntry.js',
+      },
+      exposes: {
+        './Content': './src/Content',
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+  ],
+};
---
+++ remote2/config/module-federation.js
@@ -0,0 +1,48 @@
+const deps = require('../package.json').dependencies;
+const { ModuleFederationPlugin } = require('@module-federation/enhanced/rspack');
+
+module.exports = {
+  client: new ModuleFederationPlugin({
+    name: 'remote2',
+    filename: 'remoteEntry.js',
+    remotes: {},
+    exposes: {
+      './Image': './src/Image',
+    },
+    shared: {
+      ...deps,
+      react: {
+        singleton: true,
+        requiredVersion: deps.react,
+      },
+      'react-dom': {
+        singleton: true,
+        requiredVersion: deps['react-dom'],
+      },
+    },
+  }),
+  server: [
+    new ModuleFederationPlugin({
+      remoteType: 'script',
+      runtimePlugins: [require.resolve('@module-federation/node/runtimePlugin')],
+      name: 'remote2',
+      filename: 'remoteEntry.js',
+      library: { type: 'commonjs-module' },
+      remotes: {},
+      exposes: {
+        './Image': './src/Image',
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+  ],
+};
